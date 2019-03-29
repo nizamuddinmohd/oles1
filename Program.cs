6c05ddc5f1af231604304ee68d9abeb7725e59d3
Mohd Nizamuddin <mohd.nizamuddin@iongroup.com>
2019-03-29 09:39:39 +0530

    Added UseKestrel().

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace oles1
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
